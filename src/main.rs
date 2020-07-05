ec5da2ba5aad2c8728118cf8b6fc193304f1993c
RohanGautam <rohan1gautam@gmail.com>
2020-07-05 10:46:32 +0800

    added parsing as a config trait

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!("Searching for {}", config.query);
     println!("Reading file {}", config.filename);
@@ -16,11 +16,12 @@ struct Config {
     query: String,
     filename: String,
 }
-// args can be `&[String]`  too
-fn parse_config(args: &Vec<String>) -> Config {
-    // clone creates a new copy for our struct to own.
-    let query = args[1].clone();
-    let filename = args[2].clone();
+impl Config {
+    fn new(args: &Vec<String>) -> Config {
+        // clone creates a new copy for our struct to own.
+        let query = args[1].clone();
+        let filename = args[2].clone();
 
-    Config { query, filename }
+        Config { query, filename }
+    }
 }
