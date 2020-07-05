36d4e4024295434578f85ccc7189efc1a0f53a1e
RohanGautam <rohan1gautam@gmail.com>
2020-07-05 10:44:31 +0800

    added arg parser+struct

---
+++ README.md
@@ -0,0 +1,4 @@
+- Using primitive values when a complex type would be more appropriate is an anti-pattern known as primitive obsession.
+  For example, a config variable containing all config stuff would be more appropriate than a bunch of vars scattered around
+
+> It’s better to have a working program that’s a bit inefficient than to try to hyperoptimize code on your first pass.
--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,25 @@ use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    // these are config vars, but others are logic vars.
-    // seperate logic to lib.rs, and let main be for arg parsing, config, handling error in logic.
-    let query = &args[1];
-    let filename = &args[2];
+    let config = parse_config(&args);
 
-    println!("Searching for {}", query);
-    println!("Reading file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("Reading file {}", config.filename);
     //read a file into a string
-    let contents: String = fs::read_to_string(filename).expect("Error reading the file specified");
+    let contents: String =
+        fs::read_to_string(config.filename).expect("Error reading the file specified");
     println!("Read:\n{}", contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+// args can be `&[String]`  too
+fn parse_config(args: &Vec<String>) -> Config {
+    // clone creates a new copy for our struct to own.
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config { query, filename }
 }
